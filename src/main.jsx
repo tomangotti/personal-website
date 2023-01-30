f8e47b6aa79521a5fc7f57cc4874167c06da0c71
thomas <tom.angotti11@gmail.com>
2023-01-30 10:39:44 -0700

    added homepage to package json

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HashRouter } from "react-router-dom"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-    <App />
+      <App />
     </HashRouter>
   </React.StrictMode>
 )
