6d646c6c2af2b30dbe975a4c70ca4a33c8291b53
thomas <tom.angotti11@gmail.com>
2023-01-30 11:38:35 -0700

    increase bio text to h4

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,10 @@ function About(){
     return (
         <>
         <h1>BIO</h1>
-        <h6 style={{width: "750px"}}>I am from West Virginia and attended West Virginia University. 
+        <h4 style={{width: "750px"}}>I am from West Virginia and attended West Virginia University. 
             Shortly after Graduation, I moved out to Denver, CO where I started my career as a Civil Engineer. 
             After six years, I chose to take my talents to Flatiron School in Denver and learn software engineering.
-            I am now energized and excited for the road ahead!</h6>
+            I am now energized and excited for the road ahead!</h4>
         <a href="src/resume/Thomas-Angotti Resume.pdf" download="Thomas-Angotti-resume" >Click HERE for RESUME</a>
         </>
     )
