feb4f26eb4fa07df23d94935f725ccbb940b0ce7
thomas <tom.angotti11@gmail.com>
2023-02-09 18:16:56 -0700

    added linked in

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,9 @@ function About(){
             Shortly after Graduation, I moved out to Denver, CO where I started my career as a Civil Engineer. 
             After six years, I chose to take my talents to Flatiron School in Denver and learn software engineering.
             I am now energized and excited for the road ahead!</h3>
-        <a href="src/assets/Thomas-Angotti Resume.pdf" download="Thomas-Angotti-resume" >RESUME</a>
+        <a href="src/assets/Thomas-Angotti Resume.pdf" download="Thomas-Angotti-resume" >RESUME</a><br></br>
+        <a href="https://www.linkedin.com/in/thomas-angotti/">LinkedIn</a>
+        
         </>
     )
 }
