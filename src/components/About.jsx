4b565b25a870e443878ba1760255dbf7b486493e
thomas <tom.angotti11@gmail.com>
2023-02-18 12:07:05 -0700

    added resume link and image of self

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,12 @@ function About(){
     return (
         <>
         <h1>BIO</h1>
+        <img id="self-image" src="https://media.licdn.com/dms/image/D5603AQFZiG-FTXerEw/profile-displayphoto-shrink_400_400/0/1673993772595?e=1681948800&v=beta&t=aRy2kgatkFkLbypVglftC-gTyN9QcH0yWGtnirHDcVc" />
         <h3 style={{width: "750px"}}>I am from West Virginia and attended West Virginia University. 
-            Shortly after Graduation, I moved out to Denver, CO where I started my career as a Civil Engineer. 
-            After six years, I chose to take my talents to Flatiron School in Denver and learn software engineering.
+            Shortly after Graduation, I moved out to Denver, CO where I started my career as a Civil Engineer.<br></br> 
+            After six years, I chose to take my talents to Flatiron School in Denver and learn software engineering.<br></br>
             I am now energized and excited for the road ahead!</h3>
-        <a href="src/assets/Thomas-Angotti Resume.pdf" download="Thomas-Angotti-resume" >RESUME</a><br></br>
+        <a href="https://1drv.ms/b/s!AlNHdqU_1zzeshU0nENanpknQ2d5?e=AiTKCs" >RESUME</a><br></br>
         <a href="https://www.linkedin.com/in/thomas-angotti/">LinkedIn</a>
         
         </>
